6085eb7fa64c9664664edbe01950c2fe59bf2a88
Hinano Ichikawa <ricainais1@gmail.com>
2026-03-21 21:06:27 +0900

    コメントアウト部分の削除

--- proxy.ts
+++ proxy.ts
@@ -21,27 +21,3 @@ export const config = {
     '/settings/:path*',
   ],
 }
-
-// import { NextResponse, type NextRequest } from "next/server"
-// import { updateSession } from "@/lib/supabase/proxy"
-
-// export async function proxy(request: NextRequest) {
-//   const session = await updateSession(request)
-
-// if (!session) {
-//     return NextResponse.redirect(new URL("/signin", request.url))
-//   }
-
-//   return NextResponse.next()
-// }
-
-// export const config = {
-//   matcher: [
-//     '/home/:path*',
-//         '/foods/:path*',
-//         '/newhoods/:path*',
-//         '/shopping/:path*',
-//         '/users/:path*',
-//         '/settings/:path*',
-//   ],
-// }
